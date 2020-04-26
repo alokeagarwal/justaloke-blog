6affeeda250dd84ffba64f651a6d4881bd8d914e
Aloke Agarwal <aloke_agarwal@yahoo.com>
2020-04-26 12:37:13 -0500

    First commit

---
+++ src/build/precache-manifest.8ae0adaff417bf6d97540ef4bada91b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5004f60056a36a7befabc263414b2450",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef174bcebd7d4143aaf9",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "6605ec2f6f006962bc48",
+    "url": "/static/js/2.86e6c9ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.86e6c9ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef174bcebd7d4143aaf9",
+    "url": "/static/js/main.2a100408.chunk.js"
+  },
+  {
+    "revision": "c6cd80a6fce542ddaee9",
+    "url": "/static/js/runtime-main.b9244b71.js"
+  }
+]);
